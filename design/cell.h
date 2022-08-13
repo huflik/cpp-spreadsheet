9e086d648ac32dbb6901a86d8a78e82e3f23311c
Olga Fedorova <oflik@ya.ru>
2022-08-13 13:05:40 +0300

    Спринт 16. Итоговый проект - дизайн

--- design/cell.h
+++ design/cell.h
@@ -3,9 +3,14 @@
 #include "common.h"
 #include "formula.h"
 
+#include <functional>
+#include <unordered_set>
+
+class Sheet;
+
 class Cell : public CellInterface {
 public:
-    Cell();
+    Cell(SheetInterface& sheet);
     ~Cell();
 
     void Set(std::string text);
@@ -13,11 +18,22 @@ public:
 
     Value GetValue() const override;
     std::string GetText() const override;
+    std::vector<Position> GetReferencedCells() const override;
 
 private:
-    class Impl;
-    class EmptyImpl;
-    class TextImpl;
+    class Impl;   
+    class EmptyImpl;  
+    class TextImpl;   
     class FormulaImpl;
+    
+	bool IsCircularDependency(const Impl& impl) const;
+    void InvalidateCache();   
+    
     std::unique_ptr<Impl> impl_;
+    Sheet& sheet_;
+	// ячейки, на результат которых влияет данная
+    std::unordered_set<Cell*> dest_cells_;
+	// ячейки, влияющие на результат данной
+    std::unordered_set<Cell*> sourc_cells_;
+
 };
